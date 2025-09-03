5dea2d21b60f3999e6417a0ead4f44d9b7a246cc
Yiyun Yao <106117469+nyonyoko@users.noreply.github.com>
2025-09-03 01:58:02 +0000

    Format test_hello.py with black

--- week1/test_hello.py
+++ week1/test_hello.py
@@ -3,8 +3,7 @@ from hello import say_hello, add
 
 def test_say_hello():
     assert (
-        say_hello("Leo")
-        == "Hello, Leo, welcome to Data Engineering Systems (IDS 706)!"
+        say_hello("Leo") == "Hello, Leo, welcome to Data Engineering Systems (IDS 706)!"
     )
 
 
